0f9ec74ab92f6a8f5640c65719c326e054b88a58
Poorshad Shaddel <43247296+pshaddel@users.noreply.github.com>
2020-07-23 20:03:41 +0430

    Initialize

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = (req, res, next) => {
+    const method = req.method.toLowerCase();
+    const address = process.env.baseURL ? process.env.baseURL  + req.originalUrl :  module.parent.path + "/src" + req.originalUrl;
+    try {
+        const singleRoute = (address[address.length -1] == '/') ? address +'route' : address +'/route'
+        require(singleRoute)[method](req, res, next);
+    }
+    catch{
+        try{
+            const routeMethod = (address[address.length -1] == '/') ? address + 'route.' + method : address + '/route.' + method
+            require(routeMethod)(req, res, next);
+        }
+        catch{
+            res.status(404).send("simple-express couldn't find your file")
+        }
+    }
+    next();
+}
