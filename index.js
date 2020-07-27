2c485c80193eb495ea742ade70cbe8eb9d74488c
Poorshad <p.shaddel@gmail.com>
2020-07-27 17:31:59 +0430

    handle users/userId

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
+const defaultPath = "src";
+const defaultRouteFileName = "route";
+
 module.exports = (req, res, next) => {
     const method = req.method.toLowerCase();
-    const address = (process.env.baseURL ? process.env.baseURL  + req.originalUrl :  module.parent.path + "/src" + req.originalUrl).toLowerCase().split("?")[0];
+    const address = (module.parent.path + `/${defaultPath}` + req.originalUrl).toLowerCase().split("?")[0];
+    const haveSlash = address[address.length -1] == '/';
     try {
-        const singleRoute = (address[address.length -1] == '/') ? address +'route' : address +'/route'
+        const singleRoute = haveSlash ? address + defaultRouteFileName : address + '/' + defaultRouteFileName
         require(singleRoute)[method](req, res, next);
     }
-    catch{
-        try{
-            const routeMethod = (address[address.length -1] == '/') ? address + 'route.' + method : address + '/route.' + method
+    catch {
+        try {
+            const routeMethod = haveSlash ? address + `${defaultRouteFileName}.` + method : address + `/${defaultRouteFileName}.` + method
             require(routeMethod)(req, res, next);
         }
-        catch{
-            res.status(404).send("Not Found")
+        catch {
+            try {
+                const { newAddress, id } = getIdFileAddress(address)
+                require(newAddress)[method](req, res, next, id);
+            }
+            catch (error) {
+                console.error(error)
+                res.status(404).send("Not Found");
+            }
         }
     }
     next();
+}
+
+const getIdFileAddress = (address) => {
+    let newAddress = address.split("/")
+    newAddress.pop();
+    newAddress = newAddress.join("/")
+    newAddress = newAddress[newAddress.length - 1] == "/" ? newAddress + "[id]" : newAddress + "/[id]"
+    let id = address.split("/")[address.split("/").length - 1];
+    return { id, newAddress };
 }
