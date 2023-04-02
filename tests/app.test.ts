e3270d1027c7368ea20beec4e69e48d1b09a930d
Poorshad <p.shaddel@gmail.com>
2023-04-02 12:32:54 +0200

    add one more test

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -13,43 +13,57 @@ describe("sample_project", () => {
     expect(response.body).toEqual({ message: "users router: index.route.js" });
   });
 
-  it('folders with :something must be considered', async () => {
-    const response = await request(app).get('/posts/Cal_Newport');
+  it("folders with :something must be considered", async () => {
+    const response = await request(app).get("/posts/Cal_Newport");
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: 'Posts Router - GET Method', author: 'Cal_Newport' });
-  })
+    expect(response.body).toEqual({
+      message: "Posts Router - GET Method",
+      author: "Cal_Newport",
+    });
+  });
 
-  it('folders with :something must be considered with other http methods', async () => {
-    const response = await request(app).put('/posts/Cal_Newport');
+  it("folders with :something must be considered with other http methods", async () => {
+    const response = await request(app).put("/posts/Cal_Newport");
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: 'Posts Router - PUT Method', author: 'Cal_Newport' });
-  })
+    expect(response.body).toEqual({
+      message: "Posts Router - PUT Method",
+      author: "Cal_Newport",
+    });
+  });
   // "Posts Router Base - GET Method"
-  it('should be able to have a route in each folder and nested ones', async () => {
-    const response = await request(app).get('/posts');
+  it("should be able to have a route in each folder and nested ones", async () => {
+    const response = await request(app).get("/posts");
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: 'Posts Base router: posts.js' });
-  })
+    expect(response.body).toEqual({ message: "Posts Base router: posts.js" });
+  });
 
-  it('should be able to handle plain functions too(not a express router)', async () => {
-    const response = await request(app).get('/healthcheck');
+  it("should be able to handle plain functions too(not a express router)", async () => {
+    const response = await request(app).get("/healthcheck");
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: 'Health Check Route' });
-  })
+    expect(response.body).toEqual({ message: "Health Check Route" });
+  });
 
-  describe('Should be able to handle files with http methods as extension', () => {
-    const methods = ["get", "post", "put", "delete", "patch"]
-    methods.forEach(method => {
+  describe("Should be able to handle files with http methods as extension", () => {
+    const methods = ["get", "post", "put", "delete", "patch"];
+    methods.forEach((method) => {
       it(`should be able to handle files with http methods as extension ${method.toUpperCase()}`, async () => {
-        const response = await request(app)[method]('/payments')
+        const response = await request(app)[method]("/payments");
         expect(response.status).toBe(200);
-        expect(response.body).toEqual({ message: `Payments Router - ${method.toUpperCase()} Method` });
-      })
-    })
-    it('should work in nested folders', async () => {
-      const response = await request(app).get('/payments/report')
+        expect(response.body).toEqual({
+          message: `Payments Router - ${method.toUpperCase()} Method`,
+        });
+      });
+    });
+    it("should work in nested folders", async () => {
+      const response = await request(app).get("/payments/report");
       expect(response.status).toBe(200);
-      expect(response.body).toEqual({ message: 'Payments Report Router' });
-    })
-  })
+      expect(response.body).toEqual({ message: "Payments Report Router" });
+    });
+  });
+
+  it("A combination pattern like fileName.route.get.js should be considered as a single route", async () => {
+    const response = await request(app).get("/weather");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: "Weather Router - GET Method" });
+  });
 });
---
+++ tests/sample_project/src/weather/route.get.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+    res.json({ message: 'Weather Router - GET Method' })
+}
